5bb9db3ff162440d7597e9106f5effb25a834ed7
matzekuh <matzekuh@users.noreply.github.com>
2015-07-28 18:15:39 +0200

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -13,6 +13,8 @@ $meta['database']         = array('string','_caution' => 'danger');
 $meta['charset']          = array('string','_caution' => 'danger');
 $meta['debug']            = array('multichoice','_choices' => array(0,1,2),'_caution' => 'security');
 
+$meta['TablesToLock']     = array('array','_caution' => 'danger');
+
 $meta['checkPass']        = array('','_caution' => 'danger');
 $meta['getUserInfo']      = array('','_caution' => 'danger');
 $meta['getGroups']        = array('','_caution' => 'danger');
