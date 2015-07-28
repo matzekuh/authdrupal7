ee339c52d1257c94e2e8b054a2e5033112f25ed9
matzekuh <matzekuh@users.noreply.github.com>
2015-07-28 16:19:39 +0200

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * English language file for authdrupal7 plugin
+ *
+ * @author Matthias Jung <matzekuh@web.de>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
