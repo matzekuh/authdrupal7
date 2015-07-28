6610ababeb1e7a5356e1fdf47c4291973bb423a9
matzekuh <matzekuh@users.noreply.github.com>
2015-07-28 18:18:29 +0200

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -19,3 +19,8 @@ $conf['TablesToLock']     = array();
 $conf['checkPass']        = 'SELECT pass FROM %{drupal_prefix}users WHERE name=\'%{user}\'';
 $conf['getUserInfo']      = 'SELECT name, mail FROM %{drupal_prefix}users WHERE name=\'%{user}\'';
 $conf['getGroups']        = 'SELECT roles.name FROM %{drupal_prefix}users users INNER JOIN %{drupal_prefix}users_roles userroles INNER JOIN %{drupal_prefix}role roles WHERE users.uid = userroles.uid AND roles.rid = userroles.rid AND users.name = \'%{user}\''
+
+$conf['FilterLogin']      = '';
+$conf['FilterName']       = '';
+$conf['FilterEmail']      = '';
+$conf['FilterGroup']      = '';
