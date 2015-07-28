48fefc3b69272db8e6c01d4dc8650cbc38d30b72
matzekuh <matzekuh@users.noreply.github.com>
2015-07-28 17:33:58 +0200

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -6,5 +6,11 @@
  */
 
 
-//$meta['fixme'] = array('string');
+$meta['server']           = array('string','_caution' => 'danger');
+$meta['user']             = array('string','_caution' => 'danger');
+$meta['password']         = array('password','_caution' => 'danger');
+$meta['database']         = array('string','_caution' => 'danger');
+$meta['charset']          = array('string','_caution' => 'danger');
+$meta['debug']            = array('multichoice','_choices' => array(0,1,2),'_caution' => 'security');
 
+$meta['checkPass']        = array('','_caution' => 'danger');
