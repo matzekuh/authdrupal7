cdc30a059862ea0653d16f06178a65ec1e9b6b5f
matzekuh <matzekuh@users.noreply.github.com>
2015-07-28 16:19:56 +0200

    Create settings.php

---
+++ lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * english language file for authdrupal7 plugin
+ *
+ * @author Matthias Jung <matzekuh@web.de>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
