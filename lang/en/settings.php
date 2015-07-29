8e131e0de95599d6f798c50c368fbc4899537224
matzekuh <matzekuh@users.noreply.github.com>
2015-07-29 18:31:33 +0200

    Update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -6,7 +6,19 @@
  */
 
 // keys need to match the config setting name
-// $lang['fixme'] = 'FIXME';
+$lang['server'] = 'MySQL server';
+$lang['user'] = 'MySQL username';
+$lang['password'] = 'Password for user above';
+$lang['database'] = 'Name of database containing Drupal 7 tables';
+$lang['charset'] = 'Charset of database above';
+$lang['debug'] = 'Debug level: \'0\' off, \'1\' debug messages, \'2\' all SQL statements';
+$lang['drupalPrefix'] = 'Prefix of Drupal 7 tables, ending with underscore';
+$lang['drupalRoot'] = 'Drupal 7 root directory, ending with slash';
+$lang['TablesToLock'] = 'Drupal 7 tables that should be locked during access';
+$lang['checkPass'] = 'SQL statement for retrieving password hash for given username';
+$lang['getUserInfo'] = 'SQL statement for retrieving name and mail for given username';
+$lang['getGroups'] = 'SQL statement for retrieving role names a given user is member of';
+$lang['getUserCount'] = 'SQL statement for getting number of non-deactivated users, return in field num';
 
 
 
